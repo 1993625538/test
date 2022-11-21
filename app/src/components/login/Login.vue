4278a02dfd5e213be4cfa82219c8f2490026942e
yanfeng <yf@leextx.com>
2022-11-21 17:52:13 +0800

    11

---
+++ app/src/components/login/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="login-box">
+    <button @click="callApp">呼dsffsd叫</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { phone } from "../../phone/phone";
+
+export default defineComponent({
+  setup () {
+    phone.answer('login',(data:object)=>{
+      console.log(data);
+    })
+    const callApp=()=>{
+      phone.call('loginToApp',{msg:"我是login"})
+    }
+    return {callApp}
+  }
+})
+</script>
+
+<style scoped>
+.login-box{
+    position: absolute;
+    width: 20rem;
+    height: 20rem;
+    right: 0;
+    top: 0;
+}
+</style>
